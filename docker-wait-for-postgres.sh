951021bee43254184f5601c813816ebcac8fba62
Lisa Kim <lisakeem@outlook.com>
2019-05-15 20:07:05 -0700

    Overwrite ENTRYPOINT for user and gateway

--- docker-wait-for-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "HELLO?!?!?!"
-while true; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
--- docker-wait-for-usersvc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-until docker inspect -f {{.State.Running}} dev-user-svc; do
-sleep 1
-done
-
