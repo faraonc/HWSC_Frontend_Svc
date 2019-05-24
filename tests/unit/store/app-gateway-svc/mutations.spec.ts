bdfa3a82fc02ff6a1b97d0f8dd34732bb56c5c65
Lisa Kim <lisakeem@outlook.com>
2019-05-24 16:27:51 -0700

    Remove comment and fix imports on mutation.spec

--- tests/unit/store/app-gateway-svc/mutations.spec.ts
+++ tests/unit/store/app-gateway-svc/mutations.spec.ts
@@ -1,8 +1,7 @@
 import { mutations } from '@/store/modules/app-gateway-svc/mutations';
 import { state } from '@/store/modules/app-gateway-svc/states';
-import * as action from '@/store/modules/app-gateway-svc/types-actions';
 import * as mutation from '@/store/modules/app-gateway-svc/types-mutations';
-
+import * as types from '@/store/root-types';
 
 describe(mutation.SET_HTTP_HEADER, () => {
   const { SET_HTTP_HEADER } = mutations;
@@ -16,15 +15,15 @@ describe(mutation.SET_HTTP_HEADER, () => {
 
 // https://github.com/grpc/grpc-web/pull/540
 // need to wait for next release for grpc-web
-// describe(mutation.SET_GRPC_CLIENT, () => {
-//   const { SET_GRPC_CLIENT } = mutations;
-//
-//   it('should equal to AppGatewayServiceClient object', () => {
-//     let hostname: string | undefined = process.env.VUE_APP_HOST_NAME;
-//     hostname = hostname == null ? '' : hostname;
-//     const client: AppGatewayServiceClient = new AppGatewayServiceClient(hostname, null, null);
-//     console.log('-----', client)
-//     SET_GRPC_CLIENT(state, client);
-//     expect(state.httpHeader).toEqual(client);
-//   });
-// });
+describe(mutation.SET_GRPC_CLIENT, () => {
+  const { SET_GRPC_CLIENT } = mutations;
+
+  it('should equal to AppGatewayServiceClient object', () => {
+    let hostname: string | undefined = process.env.VUE_APP_HOST_NAME;
+    hostname = hostname == null ? '' : hostname;
+    const client: types.AppGatewayServiceClient = new types.AppGatewayServiceClient(hostname, null, null);
+
+    SET_GRPC_CLIENT(state, client);
+    expect(state.httpHeader).toEqual(client);
+  });
+});
