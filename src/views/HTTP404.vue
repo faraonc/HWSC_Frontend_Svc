3fe4c16f2dbbb75715bc3fa53ed7a364505e0e79
faraonc <faraonc@outlook.com>
2019-07-28 14:41:43 -0700

    Use composition style instead of class components and decorators
    This is due to Vue not supporting the class API in the future https://github.com/vuejs/rfcs/pull/17#issuecomment-494242121

---
+++ src/views/HTTP404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ $t('http_404') }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HTTP404',
+};
+</script>
+
+<style scoped>
+
+</style>
