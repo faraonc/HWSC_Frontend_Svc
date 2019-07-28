efe9cddaa9b092f8fa47a793110247396d9ca4c4
faraonc <faraonc@outlook.com>
2019-07-28 00:50:11 -0700

    Use http 404 not found component for history mode

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="home">
-    <HelloI18n/>
-    <img alt="Vue logo" src="../assets/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import HelloI18n from '@/components/HelloI18n.vue';
-
 
 @Component({
   components: {
     HelloWorld,
-    HelloI18n,
   },
 })
 export default class Home extends Vue {
